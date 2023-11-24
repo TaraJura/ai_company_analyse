9873fbf20edd81d5c107f83bf7843e376233411b
novakj <jirkanovakneda11@seznam.cz>
2023-11-24 09:04:29 +0100

    javascript fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://cdn.skypack.dev/@popperjs/core@2.11.6/dist/esm/popper.js", preload: true
