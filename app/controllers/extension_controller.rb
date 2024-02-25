c239fe83d3445b73277f813cab2360c924057772
root <jirkanovakneda11@seznam.cz>
2024-02-25 22:06:37 +0000

    x

--- app/controllers/extension_controller.rb
+++ app/controllers/extension_controller.rb
@@ -5,10 +5,30 @@ class ExtensionController < ApplicationController
 
   def index
     @extension_client = current_user.client
+    @unique_domains_scanned = @extension_client.extension_data.select(:url_send_from).distinct.count
+    @ip_addresses_used = @extension_client.extension_data.select(:ip_address).distinct
 
     if @extension_client.nil?
       redirect_to root_path, alert: "You don't have access to the extension, please contact support"
     end
+
+    @locations = @ip_addresses_used.map do |ip|
+      response = Typhoeus.get("http://ipinfo.io/#{ip.ip_address}/json")
+      if response.success?
+        JSON.parse(response.body).merge({"ip_address" => ip.ip_address})
+      else
+        {"ip_address" => ip.ip_address, "error" => "Failed to fetch location"}
+      end
+    end
+
+
+
+
+
+
+
+
+
 
   end
 
@@ -17,8 +37,9 @@ class ExtensionController < ApplicationController
       battery_level = params[:battery_info][:batteryLevel]
       is_charging = params[:battery_info][:isCharging]
 
-      client = Client.find_or_create_by!(token_identifier: params[:token_identifier], ip_address: request.remote_ip)
-      client.extension_data.create!(data: params[:data], url_send_from: params[:url_send_from], user_agent: params[:user_agent], battery_level:, is_charging:)
+      client = Client.find_or_create_by!(token_identifier: params[:token_identifier])
+      client.extension_data.create!(data: params[:data], url_send_from: params[:url_send_from], user_agent: params[:user_agent], battery_level:, is_charging:,
+                                    ip_address: request.remote_ip)
     rescue StandardError => e
       render json: { message: "Error in extension_controller line 12" }
       return
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_052851) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_25_211450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
     t.string "token_identifier"
-    t.inet "ip_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,6 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_25_052851) do
     t.string "battery_level"
     t.boolean "is_charging"
     t.string "user_agent"
+    t.inet "ip_address"
     t.index ["client_id"], name: "index_extension_data_on_client_id"
   end
 
