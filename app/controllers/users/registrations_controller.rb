90eb4054f108a28cf6d0b942ab28c375603b3d03
TaraJura <jirkanovakneda11@seznam.cz>
2025-01-02 19:38:25 +0100

    reg update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # Prevent access to the new registration form
-  def new
-    redirect_to root_path, alert: "Registration is currently disabled."
-  end
 
   # Prevent creating new registrations
   def create
-    redirect_to root_path, alert: "Registration is currently disabled."
+    redirect_back(fallback_location: root_path, alert: "Registration is currently disabled.")
+
   end
 end
